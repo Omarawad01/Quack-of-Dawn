ff66652e1604bd647fe66977d5c81b11c2fe9dd2
Juan Enriquez <juan03192001@gmail.com>
2025-03-12 19:59:06 -0700

    Minor update

--- Assets/Scripts/Class Scripts/SceneController.cs
+++ Assets/Scripts/Class Scripts/SceneController.cs
@@ -19,7 +19,7 @@ public class SceneController : MonoBehaviour
         if(enemy == null)
         {
             enemy = Instantiate(enemyPrefab) as GameObject;
-            enemy.transform.position = new Vector3(0, 1, 0);
+            enemy.transform.position = new Vector3(84, 43, 95);
             float angle = Random.Range(0, 360);
             enemy.transform.Rotate(0, angle, 0);
         }
